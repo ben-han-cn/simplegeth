a667752d787dff3e64e5670ee26a4cfb9fb09948
ben <ben.han.cn@gmail.com>
2017-06-29 17:09:18 +0800

    replace the abbreviate git command with full command

--- install_geth.sh
+++ install_geth.sh
@@ -1,11 +1,12 @@
 sudo add-apt-repository ppa:ethereum/ethereum
 sudo apt-get update
-sudo apt-get install solc
+sudo apt-get install -y solc
 
 #admin.setSolc("/usr/bin/solc")
 
 
 git clone https://github.com/ethereum/go-ethereum.git
-git co release/1.5
+cd go-ethereum
+git checkout release/1.5
 sudo apt-get install -y build-essential 
 make all
